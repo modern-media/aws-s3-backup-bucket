bbb528bc6ea81d78e142ace48f7ce8aedfc06c07
Christopher Carson <chris@modernmedia.co>
2014-04-24 12:55:53 -0500

    added README

---
+++ README.md
@@ -0,0 +1,4 @@
+Amazon Web Services S3 Backup Bucket
+====================
+
+Back up an Amazon Web Services S3 bucket to your local machine from the command line.
--- backup.php
+++ backup.php
@@ -1,9 +1,39 @@
 <?php
 use Aws\S3\S3Client;
 require_once __DIR__ . '/vendor/autoload.php';
-var_dump($argv);
 
-$client = S3Client::factory(array(
-	'key'    => '<aws access key>',
-	'secret' => '<aws secret key>'
+date_default_timezone_set('UTC');
+
+//$argv[1] should be the credentials file
+$crd = require $argv[1];
+$client = S3Client::factory($crd);
+
+//$argv[2] should be the bucket name
+$bucket = $argv[2];
+
+//$argv[3] should be an existing directory
+$parent_dir = $argv[3];
+
+if (! is_dir($parent_dir) || ! is_writable($parent_dir)){
+	throw new Exception('The directory you specified does not exist or is not writable.');
+}
+
+$path = realpath($parent_dir);
+$path .= DIRECTORY_SEPARATOR . $bucket;
+if (! is_dir($path)){
+	mkdir($path);
+}
+$client->downloadBucket($path, $bucket);
+
+
+/**
+$iterator = $client->getIterator('ListObjects', array(
+	'Bucket' => $bucket
 ));
+
+$client->downloadBucket('/local/directory', 'my-bucket');
+
+foreach ($iterator as $object) {
+	echo $object['Key'] . "\n";
+}
+*/
